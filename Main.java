cce80ec6175803bcf3b06a4ffacb44a5ec1c6a96
Guilherme Paiva Alves <guipaiva.alves14@gmail.com>
2026-08-08 19:59:52 +0000

    Fazendo a primeira tesk

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        String nome;
+        String turma;
+        String curso;
+        Scanner read  = new Scanner(System.in);
+
+        System.out.println("Digite seu nome:");
+        nome = read.nextLine();
+
+        System.out.println("Digite o nome da turma:");
+        turma = read.nextLine();
+
+        System.out.println("Digite o nome do curso:");
+        curso = read.nextLine();
+
+        System.out.printf("Nome: %s\n Turma: %s\n Curso: %s\n",nome,turma,curso);
     }
 }
